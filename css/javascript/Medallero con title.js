13d01eeddcee65ba26e1203cad287a3ca28d5817
multiverserpg230124-alt <multiverserpg230124@gmail.com>
2026-08-16 03:21:34 -0600

    Create Medallero con title.js

---
+++ css/javascript/Medallero con title.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+
+$('img[src="https://cdn-icons-png.flaticon.com/512/1033/1033071.png"]').attr('title', 'Moneda de Mario Bros');
+$('img[src="https://cdn-icons-png.flaticon.com/512/1033/1033083.png"]').attr('title', 'Pokeball');
+
+});
